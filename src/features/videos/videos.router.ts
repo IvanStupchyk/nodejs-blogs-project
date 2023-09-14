2582b297d425f7897bec6cca5dfc272dc37bf311
i.stupchyk <i.stupchyk@godeltech.com>
2023-09-14 22:47:07 +0200

    minor mixes

--- src/features/videos/videos.router.ts
+++ src/features/videos/videos.router.ts
@@ -40,9 +40,9 @@ export const getVideosRouter = (db: DBType) => {
 
     const createdAt: Date = new Date()
 
-    // let publicationDate: Date = new Date(createdAt)
-    // publicationDate.setDate(createdAt.getDate() + 1)
-    let publicationDate = new Date(createdAt.getTime() + 86400000)
+    let publicationDate: Date = new Date(createdAt)
+    publicationDate.setDate(createdAt.getDate() + 1)
+    // publicationDate = new Date(createdAt.getTime() + 86400000)
 
     const createdVideo: VideoType = {
       id: 32,
