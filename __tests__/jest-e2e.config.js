d5a0d78c83cd54c0aa03d3d8c440ce59607e248c
i.stupchyk <i.stupchyk@godeltech.com>
2023-09-27 14:38:22 +0200

    increased time for all test cases in the jest config

--- __tests__/jest-e2e.config.js
+++ __tests__/jest-e2e.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	modulePathIgnorePatterns: ["<rootDir>/dist/"],
-	testRegex: ".e2e.ts$"
+	testRegex: ".e2e.ts$",
+	testTimeout: 10000
 }
